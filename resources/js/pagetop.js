8db536b7511904833d0d45786ae3b3990b255502
supilog <supi.gm+supilog@gmail.com>
2024-07-26 14:02:12 +0900

    update

---
+++ resources/js/pagetop.js
@@ -0,0 +1,14 @@
+$(function () {
+    var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({scrollTop: 0}, 500);
+        return false;
+    });
+});
