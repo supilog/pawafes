26fe08c89348d1c1c4ca4d710cf28312159d05f9
shigyamamoto <supi@SupisAir2022.local>
2024-07-28 00:34:26 +0900

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
 <div class="container mb-2">
     <div class="entrance">
         <a href="{{ route('list') }}">
-            <button type="button" class="btn btn-outline-secondary pfentrance">パワフェス図鑑埋め用メモ&nbsp;<i class="fi-arrow-right"></i></button>
+            <button type="button" class="btn btn-outline-secondary pfentrance"><i class="fi-arrow-right"></i>&nbsp;パワフェス図鑑埋め用メモ&nbsp;<i class="fi-arrow-left"></i></button>
         </a>
     </div>
     <div class="notes">
@@ -24,14 +24,14 @@
             【注意1】
         </div>
         <div>
-            獲得選手記録をCookieに保存しているので、Cookieを無効にした状態では使用することができません。<br>
+            <span class="red">獲得選手記録をCookieに保存しているので、Cookieを無効にした状態では使用することができません。</span><br>
         </div>
         <div>
             【注意2】
         </div>
         <div>
             本アプリはゲームエイト様の選手図鑑データ(<a href="https://game8.jp/pawapuro2024-2025/625623">https://game8.jp/pawapuro2024-2025/625623</a>)を参考にしており、<br>
-            2024/07/25時点では図鑑が未完成となっているため、本アプリでも一部データは欠損しておりますので、ご了承ください。<br>
+            2024/07/28時点では図鑑が未完成となっているため、本アプリでも一部データは欠損しておりますので、ご了承ください。<br>
         </div>
         <div class="data-delete">
             <a href="#" id="storage-clean">データをクリアする(クリアしたデータは戻せません)</a>
